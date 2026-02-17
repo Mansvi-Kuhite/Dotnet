69ee589f3bc56778e0c5ffca392589f1921c1d07
Manasvi Kuhite <manasvikuhite@gmail.com>
2026-02-17 20:34:13 +0530

    Removed old folder name

--- Day-4/Lab5/ITransformable.cs
+++
@@ -1,7 +0,0 @@
-namespace OOPFundamentals.Lab5
-{
-    public interface ITransformable
-    {
-        void Scale(double factor);
-    }
-}
--- Day-4/Lab5/Shape.cs
+++
@@ -1,7 +0,0 @@
-namespace OOPFundamentals.Lab5
-{
-    public abstract class Shape
-    {
-        public abstract double Area();
-    }
-}
